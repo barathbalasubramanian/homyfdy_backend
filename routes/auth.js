d1f980a682e845346c18744c3cc21a639232cc65
barathbalasubramanian <105224717+barathbalasubramanian@users.noreply.github.com>
2024-09-11 12:30:52 +0530

    1d ..

--- routes/auth.js
+++ routes/auth.js
@@ -15,7 +15,7 @@ router.post("/login", async (req, res) => {
         number:number,
         city:city
     }, "dcce5c8476a044a410939dc30cbd82353281d7b09859fe8eec662be1e21753dc", {
-      expiresIn: "1h",
+      expiresIn: "1d",
     });
     res.status(200).json({ token });
   } catch (error) {
